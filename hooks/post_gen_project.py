56f89d3471a1c7cd1e1412cf461528cf1e7b2cce
firdousreshi <62369925+firdousreshi@users.noreply.github.com>
2024-01-30 12:07:28 +0530

    Update post_gen_project.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -14,25 +14,25 @@ if "{{cookiecutter.initialize_git}}":
 else:
     pass
 
-# print(f"----- [*] GENERATING PROJECT [*] ----- {{cookiecutter.setup_pre_commit_hooks}}")
+# # print(f"----- [*] GENERATING PROJECT [*] ----- {{cookiecutter.setup_pre_commit_hooks}}")
+# # if "{{cookiecutter.setup_pre_commit_hooks}}":
+# #     subprocess.run(["pre-commit", "install", "--install-hooks"])
+# # else:
+# #     subprocess.run(["rm", ".pre-commit-config.yml"])
+
+
+# # Check if setup_pre_commit_hooks is True
 # if "{{cookiecutter.setup_pre_commit_hooks}}":
-#     subprocess.run(["pre-commit", "install", "--install-hooks"])
+#     try:
+#         # Attempt to install pre-commit hooks
+#         subprocess.run(["pre-commit", "install", "--install-hooks"], check=True)
+#     except subprocess.CalledProcessError as e:
+#         # Handle error if pre-commit installation fails
+#         print("Error installing pre-commit hooks:", e)
 # else:
-#     subprocess.run(["rm", ".pre-commit-config.yml"])
-
-
-# Check if setup_pre_commit_hooks is True
-if "{{cookiecutter.setup_pre_commit_hooks}}":
-    try:
-        # Attempt to install pre-commit hooks
-        subprocess.run(["pre-commit", "install", "--install-hooks"], check=True)
-    except subprocess.CalledProcessError as e:
-        # Handle error if pre-commit installation fails
-        print("Error installing pre-commit hooks:", e)
-else:
-    try:
-        # Attempt to remove .pre-commit-config.yml
-        subprocess.run(["rm", ".pre-commit-config.yml"], check=True)
-    except subprocess.CalledProcessError as e:
-        # Handle error if removal fails
-        print("Error removing .pre-commit-config.yml:", e)
+#     try:
+#         # Attempt to remove .pre-commit-config.yml
+#         subprocess.run(["rm", ".pre-commit-config.yml"], check=True)
+#     except subprocess.CalledProcessError as e:
+#         # Handle error if removal fails
+#         print("Error removing .pre-commit-config.yml:", e)
